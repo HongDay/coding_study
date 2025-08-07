1b33f8d04374e9c6a83def038887d1e4be4b950e
kmj7769 <155081737+kmj7769@users.noreply.github.com>
2025-08-07 22:33:47 +0900

    Update KmjProb5.java
    rename class

--- prob5/KmjProb5.java
+++ prob5/KmjProb5.java
@@ -2,7 +2,7 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.IOException;
 
-public class Main {
+public class KmjProb5 {
     static Integer[][] arr = new Integer[41][2];
 
     public static void main(String[] args) throws IOException {
