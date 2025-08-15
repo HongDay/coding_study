b5f818bb11c0b1ed6472150ce3cbea446171ac9a
tn-als <kusumin38@gmail.com>
2025-08-16 03:25:22 +0900

    prob no.11 solve

--- prob11/KsmProb11.java
+++ prob11/KsmProb11.java
@@ -14,11 +14,13 @@ public class KsmProb11 {
     private void zelda() throws IOException {
         StringBuilder sb = new StringBuilder();
 
+        // 문제 번호
         int index = 1;
         while (true) {
             int n = Integer.parseInt(br.readLine());
             if (n == 0) break;
 
+            // 입력받은 도둑루피 저장
             int[][] cave = new int[n][n];
             for (int i = 0; i < n; i++) {
                 StringTokenizer st = new StringTokenizer(br.readLine());
@@ -37,10 +39,14 @@ public class KsmProb11 {
     }
 
     private int bfs(int[][] cave) {
+        // 다익스트라 알고리즘을 위한 우선순위 큐
+        // cave의 각 인덱스 값을 비교하여 오름차순으로 정렬
         PriorityQueue<Point> q = new PriorityQueue<>(
                 Comparator.comparingInt(p -> cave[p.y][p.x])
         );
 
+        // 입력 도둑루피 값이 0이상 9이하로 0이 나올 수도 있기 때문에
+        // 초기 값은 null로 하고자 Integer 사용
         Integer[][] rupee = new Integer[cave.length][cave.length];
 
         q.add(new Point(0, 0));
@@ -49,16 +55,22 @@ public class KsmProb11 {
         while (!q.isEmpty()) {
             Point p = q.poll();
 
+            // 목표 위치면 아래의 반복문 돌지 않고 바로 종료
             if (p.x == cave.length - 1 && p.y == cave.length - 1) continue;
 
             for (int i = 0; i < 4; i++) {
                 int nextX = p.x + distX[i];
                 int nextY = p.y + distY[i];
 
+                // 배열 범위 벗어나면 continue
                 if (nextX < 0 || nextX >= cave.length || nextY < 0 || nextY >= cave.length) continue;
 
+                // 다음 위치에 갈 때 현재 위치 값을 더한 값
                 int addRupee = rupee[p.y][p.x] + cave[nextY][nextX];
 
+                // 다음 위치의 루피가 배열에 아직 저장되지 않았거나
+                // 다음 위치의 루피 값보다 addRupee가 작다면
+                // 다음 위치의 루피 값을 addRupee로 변경하고 큐에 해당 인덱스 넣음
                 if(rupee[nextY][nextX] == null || rupee[nextY][nextX] > addRupee){
                     rupee[nextY][nextX] = addRupee;
                     q.add(new Point(nextX, nextY));
