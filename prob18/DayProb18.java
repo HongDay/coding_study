c322ff6e41a7a43c38310ece81e4ba659085ca31
Day HONG <tot032874@gmail.com>
2025-08-25 18:42:24 +0900

    problem 18 first attempt

--- prob18/DayProb18.java
+++ prob18/DayProb18.java
@@ -1,4 +1,75 @@
 package prob18;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 public class DayProb18 {
+
+    private static int N;
+    private static int M;
+    private static int H;
+    private static boolean[][] garo;
+    private static int answer;
+
+    public static void main (String[] argv) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        H = Integer.parseInt(st.nextToken());
+
+        garo = new boolean[H][N-1];
+
+        for (int i = 0; i < M; i++){
+            st = new StringTokenizer(br.readLine());
+            int hori = Integer.parseInt(st.nextToken()) - 1;
+            int verti = Integer.parseInt(st.nextToken()) - 1;
+            garo[hori][verti] = true;
+        }
+
+        answer = 4;
+        makeLadder(0,0);
+        if (answer == 4){answer = -1;}
+        System.out.print(answer);
+
+    }
+
+    private static void makeLadder (int count, int idx) {
+        if (count > 3 || count >= answer) {return;}
+        if (playLadder()) {
+            answer = count;
+            return;
+        }
+
+        for (int i = idx; i < H*(N-1); i++){
+            int row = i / (N-1);
+            int col = i % (N-1);
+
+            if (garo[row][col]){continue;}
+            if (col-1 >= 0 && garo[row][col-1]){continue;}
+            if (col+1 < N-1 && garo[row][col+1]){continue;}
+
+            garo[row][col] = true;
+            makeLadder(count + 1, i+1);
+            garo[row][col] = false;
+        }
+    }
+
+    private static boolean playLadder() {
+        for (int i = 0; i < N; i++){
+            int now = i;
+            for (int j = 0; j < H; j++){
+                if (now - 1 >= 0 && garo[j][now - 1]){
+                    now -= 1;
+                } else if (now < N-1 && garo[j][now]) {
+                    now += 1;
+                }
+            }
+            if (now != i) {return false;}
+        }
+        return true;
+    }
+
 }
