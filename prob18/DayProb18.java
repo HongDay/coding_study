fd5a254ff0df385ac3f2a27be8f1ad7de3c0325d
Day HONG <tot032874@gmail.com>
2025-08-25 22:02:14 +0900

    problem 18 second attempt

--- prob18/DayProb18.java
+++ prob18/DayProb18.java
@@ -29,21 +29,34 @@ public class DayProb18 {
             garo[hori][verti] = true;
         }
 
-        answer = 4;
-        makeLadder(0,0);
-        if (answer == 4){answer = -1;}
+        answer = -1;
+        answer = makeLadder();
         System.out.print(answer);
 
     }
 
-    private static void makeLadder (int count, int idx) {
-        if (count > 3 || count >= answer) {return;}
+    private static int makeLadder () {
+        // 0
         if (playLadder()) {
-            answer = count;
-            return;
+            return 0;
         }
 
-        for (int i = idx; i < H*(N-1); i++){
+        // 1
+        for (int i = 0; i < H*(N-1); i++){
+            int row = i / (N-1);
+            int col = i % (N-1);
+
+            if (garo[row][col]){continue;}
+            if (col-1 >= 0 && garo[row][col-1]){continue;}
+            if (col+1 < N-1 && garo[row][col+1]){continue;}
+
+            garo[i/(N-1)][i%(N-1)] = true;
+            if (playLadder()){return 1;}
+            garo[row][col] = false;
+        }
+
+        // 2
+        for (int i = 0; i < H*(N-1); i++){
             int row = i / (N-1);
             int col = i % (N-1);
 
@@ -52,9 +65,61 @@ public class DayProb18 {
             if (col+1 < N-1 && garo[row][col+1]){continue;}
 
             garo[row][col] = true;
-            makeLadder(count + 1, i+1);
+
+            for (int j = i+1; j < H*(N-1); j++){
+                int row2 = j / (N-1);
+                int col2 = j % (N-1);
+
+                if (garo[row2][col2]){continue;}
+                if (col2-1 >= 0 && garo[row2][col2-1]){continue;}
+                if (col2+1 < N-1 && garo[row2][col2+1]){continue;}
+
+                garo[row2][col2] = true;
+                if (playLadder()){return 2;}
+                garo[row2][col2] = false;
+            }
             garo[row][col] = false;
         }
+
+        // 3
+        for (int i = 0; i < H*(N-1); i++){
+            int row = i / (N-1);
+            int col = i % (N-1);
+
+            if (garo[row][col]){continue;}
+            if (col-1 >= 0 && garo[row][col-1]){continue;}
+            if (col+1 < N-1 && garo[row][col+1]){continue;}
+
+            garo[row][col] = true;
+
+            for (int j = i+1; j < H*(N-1); j++){
+                int row2 = j / (N-1);
+                int col2 = j % (N-1);
+
+                if (garo[row2][col2]){continue;}
+                if (col2-1 >= 0 && garo[row2][col2-1]){continue;}
+                if (col2+1 < N-1 && garo[row2][col2+1]){continue;}
+
+                garo[row2][col2] = true;
+
+                for (int k = j+1; k < H*(N-1); k++){
+                    int row3 = k / (N-1);
+                    int col3 = k % (N-1);
+
+                    if (garo[row3][col3]){continue;}
+                    if (col3-1 >= 0 && garo[row3][col3-1]){continue;}
+                    if (col3+1 < N-1 && garo[row3][col3+1]){continue;}
+
+                    garo[row3][col3] = true;
+                    if (playLadder()){return 3;}
+                    garo[row3][col3] = false;
+                }
+                garo[row2][col2] = false;
+            }
+            garo[row][col] = false;
+        }
+
+        return -1;
     }
 
     private static boolean playLadder() {
